92dc5b0afbc8885bcb124a864cd89773cdeddeb7
Somesh Mishra <someshmishra3436@gmail.com>
2024-01-10 17:00:21 +0530

    ...

--- calculator.cpp
+++ calculator.cpp
@@ -8,7 +8,11 @@ void sum(int a, int b){
 
 cout<<"The sum is:"<<a+b;
 }
+void division(int a, int b){
+if(b==0){cout<<"Cannot divide by 0"; return;} 
+else{cout<<"The result is "<<((float)a)/(float)b;}
 
+}
 int main(){
 
 }
